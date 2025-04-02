e04cc43047f8d163d7bd4c6a944e429afb680f31
enzo agu <enzo.gustavo.agu@gmail.com>
2025-04-02 12:26:22 -0300

    clase_1

---
+++ clase_1/index.js
@@ -0,0 +1,125 @@
+// 🚀
+// let nombreUno = 'Enzo';
+// let booleanVerdadero = true;
+// let booleanFalso = false;
+// let numero = 10;
+
+// console.log(nombreUno, booleanFalso, booleanVerdadero, numero);
+
+// nombreUno = 'Juan';
+// console.log(nombreUno);
+
+// const anio = 2025;
+
+// console.log(typeof anio, typeof nombreUno);
+
+// 🚀
+// let stringDiez= '10'
+// let n1 = 10 
+// let n2 = 20 
+// const PI= 3.14
+// let resultado = n2 * PI
+
+// console.log(resultado);
+
+// 🚀
+// let pregunta = prompt('Decime tu nombre')
+// let edad = parseInt(prompt('Decime tu edad'))
+// let edadDos = Number(prompt('Decime tu edad'))
+
+
+// console.log(pregunta);
+// console.log(typeof edad);
+
+// 🚀
+// let pregunta = prompt('Decime tu nombre')
+
+// let n1 = 100 
+// let n2 = 200
+// let resultado = n1 + n2
+// alert(resultado)
+
+// let pregunta = '¿Confirma la operación?'
+
+// confirm(pregunta)
+
+
+// EJEMPLO 1 - CONDICIONALES 🚀
+
+// let edad = 36;
+
+// if(edad < 18){
+//     console.log('Usted es menor de edad');
+// }
+// if(edad === 35 ){
+//     console.log('Usted es mayor de edad');
+// }
+// else{
+//     console.log('No sabemos su edad');
+// }
+
+
+// let temperatura = 20;
+
+// if(temperatura > 28){
+//     console.log('Parece que volvió el verano');
+// }
+// else if ( temperatura < 15 ){
+//     console.log('Dia fresco');
+// }
+// else{
+//     console.log('No sabemo la temp.');
+// }
+
+
+// let profesor = '';
+
+// if (profesor === 'Juan'){
+//     console.log('Se cumple la cond.');
+// }
+// else{
+//     alert('Ese no es su nombre');
+//     let nombre = prompt('Cual es su nombre')
+//     alert(`Su nombre es ${nombre}`)
+// }
+
+
+// EJEMPLO 2 FOR 🚀
+//      DESDE - HASTA - ACT.
+// for (let i = 1; i < 5; i++) {
+//    alert(i);
+// }
+
+
+// const cantPacientes = Number(prompt('Cuantos pacientes desea registrar?'));
+
+// for (let i = 1; i <= cantPacientes; i++) {
+//     const nombrePaciente = prompt(`Nombre paciente número ${i}`)
+//     const edad = Number(prompt(`Edad del paciente número ${i}`))    
+//     const obraSocial = prompt(`Obra social del paciente ${i}`)
+
+//     console.log(`Paciente ${nombrePaciente}, de ${edad} años (${obraSocial}), registrado`);
+
+// }
+
+
+// EJEMPLO 3 WHILE 🚀
+
+// let contador = 0
+
+// while (contador <= 5) {
+
+//     alert(contador);
+//     contador++
+    
+// }
+
+
+
+
+
+
+
+
+
+
